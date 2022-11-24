ff8bf8c0e8d1d53f276d1a2d0cafbfa6c99bfef7
RonAndDumbleTheCats <112146620+RonAndDumbleTheCats@users.noreply.github.com>
2022-11-23 20:49:41 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#intro 
+print("Welcome to the adlib game!")
+print()
+print()
+#Get User Input
+noun = input("Pick a noun >>")
+verb = input("Pick a Verb")
+ProperNoun=input("Pick a Proper Noun")
+#Output a funny AdLib!
+print(ProperNoun,verb,"to the",noun)
